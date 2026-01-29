0bb85e405637c5ef5a2a7df4ac087a5f9e94e121
Manav <manavk4213@gmail.com>
2026-01-29 14:35:47 +0530

    Template setup

--- job_fair/views.py
+++ job_fair/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def form(request):
+    return render(request, "form.html")
+
+def done(request):
+    return render(request, "done.html")
