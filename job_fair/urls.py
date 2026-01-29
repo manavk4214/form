8605fc6215f8bcd5a219ba2ba82635789364a670
Manav <manavk4214@gmail.com>
2026-01-29 19:47:03 +0530

    finalising commit 1

--- job_fair/urls.py
+++ job_fair/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("",views.form,name="form"),
     path("form",views.form,name="form"),
     path("done",views.done,name="done")
 ]
