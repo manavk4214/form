cf191a457a15b4cfa918950e9dc896655f5cfe3e
Manav <manavk4213@gmail.com>
2026-01-29 15:42:38 +0530

    template side commit

--- job_fair/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobFairConfig(AppConfig):
-    name = 'job_fair'
--- job_fair/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("form",views.form,name="form"),
-    path("done",views.done,name="done")
-]
