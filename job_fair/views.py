a32cbd51baf5cbcb4005b9f7a11e887a98179ffe
pulkit <shahpulkit45@gmail.com>
2026-01-29 15:39:16 +0530

    model commit

--- job_fair/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def form(request):
-    return render(request, "form.html")
-
-def done(request):
-    return render(request, "done.html")
